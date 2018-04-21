5c41fd459bfe4b9455c2583f8d03656897a7378a
yemeishu@126.com <bjuo7bvp>
2018-04-21 21:59:09 +0800

    增加 graphQL 和 jwt 插件配置文件

---
+++ config/graphql.php
@@ -0,0 +1,129 @@
+<?php
+
+
+use App\GraphQL\Mutation\NewUserMutation;
+use App\GraphQL\Mutation\UpdateUserMutation;
+use App\GraphQL\Query\UsersQuery;
+use App\GraphQL\Type\MyProfileType;
+use App\GraphQL\Type\OrdersType;
+use App\GraphQL\Type\UsersType;
+use example\Mutation\ExampleMutation;
+use example\Query\ExampleQuery;
+use example\Type\ExampleRelationType;
+use example\Type\ExampleType;
+
+return [
+
+    // The prefix for routes
+    'prefix' => 'graphql',
+
+    // The routes to make GraphQL request. Either a string that will apply
+    // to both query and mutation or an array containing the key 'query' and/or
+    // 'mutation' with the according Route
+    //
+    // Example:
+    //
+    // Same route for both query and mutation
+    //
+    // 'routes' => 'path/to/query/{graphql_schema?}',
+    //
+    // or define each route
+    //
+    // 'routes' => [
+    //     'query' => 'query/{graphql_schema?}',
+    //     'mutation' => 'mutation/{graphql_schema?}',
+    // ]
+    //
+    'routes' => '{graphql_schema?}',
+
+    // The controller to use in GraphQL request. Either a string that will apply
+    // to both query and mutation or an array containing the key 'query' and/or
+    // 'mutation' with the according Controller and method
+    //
+    // Example:
+    //
+    // 'controllers' => [
+    //     'query' => '\Rebing\GraphQL\GraphQLController@query',
+    //     'mutation' => '\Rebing\GraphQL\GraphQLController@mutation'
+    // ]
+    //
+    'controllers' => \Rebing\GraphQL\GraphQLController::class . '@query',
+
+    // Any middleware for the graphql route group
+    'middleware' => [],
+
+    // The name of the default schema used when no argument is provided
+    // to GraphQL::schema() or when the route is used without the graphql_schema
+    // parameter.
+    'default_schema' => 'default',
+
+    // The schemas for query and/or mutation. It expects an array of schemas to provide
+    // both the 'query' fields and the 'mutation' fields.
+    //
+    // You can also provide a middleware that will only apply to the given schema
+    //
+    // Example:
+    //
+    //  'schema' => 'default',
+    //
+    //  'schemas' => [
+    //      'default' => [
+    //          'query' => [
+    //              'users' => 'App\GraphQL\Query\UsersQuery'
+    //          ],
+    //          'mutation' => [
+    //
+    //          ]
+    //      ],
+    //      'user' => [
+    //          'query' => [
+    //              'profile' => 'App\GraphQL\Query\ProfileQuery'
+    //          ],
+    //          'mutation' => [
+    //
+    //          ],
+    //          'middleware' => ['auth'],
+    //      ],
+    //      'user/me' => [
+    //          'query' => [
+    //              'profile' => 'App\GraphQL\Query\MyProfileQuery'
+    //          ],
+    //          'mutation' => [
+    //
+    //          ],
+    //          'middleware' => ['auth'],
+    //      ],
+    //  ]
+    //
+    'schemas' => [
+        'default' => [
+            'query' => [
+                'users' => UsersQuery::class,
+                'myProfile' => \App\GraphQL\Query\MyProfileQuery::class,
+            ],
+            'mutation' => [
+                'NewUser' => NewUserMutation::class,
+                'UpdateUser' => UpdateUserMutation::class
+            ],
+            'middleware' => []
+        ],
+    ],
+    'types' => [
+        'users'   => UsersType::class,
+        'orders'  => OrdersType::class,
+        'myprofile' => MyProfileType::class
+    ],
+    
+    // This callable will be passed the Error object for each errors GraphQL catch.
+    // The method should return an array representing the error.
+    // Typically:
+    // [
+    //     'message' => '',
+    //     'locations' => []
+    // ]
+    'error_formatter' => ['\Rebing\GraphQL\GraphQL', 'formatError'],
+
+    // You can set the key, which will be used to retrieve the dynamic variables
+    'params_key'    => 'params',
+    
+];
