6db2a21deafc45ef0a4de6bd88a9c6b75a555996
yemeishu@126.com <bjuo7bvp>
2018-05-30 23:43:12 +0800

    列表增加分页功能功能

--- app/Http/Controllers/RssController.php
+++ app/Http/Controllers/RssController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Common\Xpath;
 use Illuminate\Http\Request;
 use App\Xpath as XpathModel;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class RssController extends Controller
 {
@@ -13,9 +14,18 @@ class RssController extends Controller
      * 获取所有已有的 rss 列表
      * return []
      */
-    public function index()
+    public function index(Request $request)
     {
-        return  $this->output(200, '数据返回成功', ['list' => XpathModel::all()]);
+        $all_rss = XpathModel::all();
+
+        $current_page = $request->input('current_page', 1);
+        $per_page = $request->input('per_page', 3);
+
+        $items = $all_rss->forPage($current_page, $per_page);
+
+        $paginate = new LengthAwarePaginator($items, $all_rss->count(), $per_page, $current_page);
+
+        return  $this->output(200, '数据返回成功', ['rss' => $paginate]);
     }
 
     public function show($id)
--- resources/assets/js/views/Xpath.vue
+++ resources/assets/js/views/Xpath.vue
@@ -1,34 +1,58 @@
 <template>
-    <el-table
-    :data="lists"
-    stripe
-    style="width: 100%">
-    <el-table-column
-      prop="id"
-      label="ID"
-      width="90">
-    </el-table-column>
-    <el-table-column
-      prop="url"
-      label="链接">
-    </el-table-column>
-    <el-table-column
-      prop="urldesc"
-      label="简要描述"
-      width="180">
-    </el-table-column>
-     <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleRssShow(scope.$index, scope.row)">查看 RSS</el-button>
-        <el-button
-          size="mini"
-          type="success"
-          @click="handleDingyue(scope.$index, scope.row)">订阅</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+    <el-container>
+        <el-main>
+            <el-card class="box-card">
+                <div slot="header" class="clearfix">
+                    <span>操作</span>
+                    <el-button style="float: right; padding: 3px 0" type="text">如果没有你要的，点此发起申请制作</el-button>
+                </div>
+                <div>
+                    <el-table
+                        :data="list"
+                        stripe
+                        style="width: 100%">
+                        <el-table-column
+                        prop="id"
+                        label="ID"
+                        width="90">
+                        </el-table-column>
+                        <el-table-column
+                        prop="url"
+                        label="链接">
+                        </el-table-column>
+                        <el-table-column
+                        prop="urldesc"
+                        label="简要描述"
+                        width="180">
+                        </el-table-column>
+                        <el-table-column label="操作">
+                        <template slot-scope="scope">
+                            <el-button
+                            size="mini"
+                            @click="handleRssShow(scope.$index, scope.row)">查看 RSS</el-button>
+                            <el-button
+                            size="mini"
+                            type="success"
+                            @click="handleDingyue(scope.$index, scope.row)">订阅</el-button>
+                        </template>
+                        </el-table-column>
+                    </el-table>
+                </div>
+            </el-card>
+        </el-main>
+        <el-footer>
+            <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="current_page"
+                :page-sizes="page_sizes"
+                :page-size="per_page"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="total"
+                class="right">
+            </el-pagination>
+        </el-footer>
+    </el-container>
 </template>
 <script>
 import { fetchList } from '../apis/rss'
@@ -38,21 +62,35 @@ export default {
   },
   data () {
     return {
-      lists: [],
+      list: [],
       locked: false,
       has_more: true,
       next_cursor: 1,
       loading: true,
       showtop: false,
-      hash_id: ''
+      hash_id: '',
+      current_page: 1,
+      per_page: 3,
+      page_sizes: [3, 20, 30, 40],
+      total: 0
     }
   },
   methods: {
     async getList () {
-        const response = await fetchList()
-        console.log(response)
-        this.lists = response.data.data.list
-        console.log(this.lists)
+        const response = await fetchList({
+            per_page: this.per_page,
+            current_page: this.current_page
+        })
+        let rss = response.data.data.rss
+        let data = response.data.data.rss.data
+        if (data instanceof Array) {
+            this.list = data
+        } else {
+            this.list = Object.values(data)
+        }
+        this.current_page = Number(rss.current_page)
+        this.per_page = Number(rss.per_page)
+        this.total = Number(rss.total)
     },
     fetchData (to, from, next) {
       this.getList()
@@ -64,6 +102,16 @@ export default {
     handleDingyue (index, data) {
         console.log(index)
         console.log(data)
+    },
+    handleSizeChange(val) {
+        console.log(`每页 ${val} 条`)
+        this.per_page = val
+        this.getList()
+    },
+    handleCurrentChange(val) {
+        console.log(`当前页: ${val}`)
+        this.current_page = val
+        this.getList()
     }
   },
   beforeRouteEnter (to, from, next) {
@@ -74,3 +122,8 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+    .right {
+        float: right;
+    }
+</style>
