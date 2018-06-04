79e99bb6e7fbe07f8dc156420bd233652ada18d9
yemeishu@126.com <bjuo7bvp>
2018-06-04 23:29:09 +0800

    test pre-commit

--- app/Http/Controllers/ApplyController.php
+++ app/Http/Controllers/ApplyController.php
@@ -35,16 +35,6 @@ class ApplyController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -68,56 +58,13 @@ class ApplyController extends Controller
         ]);
 
         if ($apply) {
-            return $this->output(200,
+            return $this->output(
+                200,
                 "创建数据成功: $apply->id",
-                ['apply' => $apply]);
+                ['apply' => $apply]
+            );
         }
 
         return $this->output(502, '创建失败');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
